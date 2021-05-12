7d7655164ade96c80e789ef4de0862d77f64c5ee
Amir Aizin <amiraizin20@gmail.com>
2021-05-12 23:46:26 +0300

    added controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,32 +1,11 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
-import Product from "../models/productModal.js";
+import {
+  getProductById,
+  getProducts,
+} from "../controllers/productController.js";
 const router = express.Router();
 
-// @desc fetch all products
-// @route GET /api/products
-// @access Public route
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-// @desc fetch a single product by id
-// @route GET /api/products/:id
-// @access Public route
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 export default router;
