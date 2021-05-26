1d5a17aa2fb53ebac91ffe8f0def7c735f45af81
Amir Aizin <amiraizin20@gmail.com>
2021-05-26 23:18:18 +0300

    added update an existing product

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -100,7 +100,7 @@ const ProductListScreen = ({ history }) => {
                 <tr key={product._id}>
                   <td>{product._id}</td>
                   <td>{product.name}</td>
-                  <td>${products.price}</td>
+                  <td>${product.price}</td>
                   <td>{product.category}</td>
                   <td>{product.brand}</td>
                   <td>
