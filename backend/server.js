7a489bc940a5f981243b23b5a2f1ed95d2acb84b
Amir Aizin <amiraizin20@gmail.com>
2021-05-29 22:45:03 +0300

    prepare for deploment

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,6 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json()); //allow to accept json in body
 
-app.get("/", (req, res) => {
-  res.send("API IS RUNNING ...");
-});
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -30,10 +27,20 @@ app.use("/api/upload", uploadRoutes);
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
-
 //Making the uploads folder to be static using express
 const __dirname = path.resolve(); //__dirname works only with require
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API IS RUNNING ...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
